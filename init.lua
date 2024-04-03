b800b21dfbddfaf0e826bbdb4f5ec450f58a3026
Nikita <chucheloff@gmail.com>
2024-04-03 13:30:19 +0500

    disable disabling arrow keys in insesrt mode

--- init.lua
+++ init.lua
@@ -43,7 +43,7 @@ vim.opt.smartcase = true
 vim.opt.signcolumn = 'yes'
 
 -- display colored column to indicate a preffred file line length
-vim.opt.colorcolumn = '80'
+vim.opt.colorcolumn = '88'
 
 -- Decrease update time
 vim.opt.updatetime = 250
@@ -117,6 +117,11 @@ vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- TIP: Disable arrow keys in normal mode
+-- vim.keymap.set('i', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set('i', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set('i', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
