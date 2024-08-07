f30cc3cf207db8d6458e522bd3238a6255412ee0
Nikita Chuchelov <chucheloff@gmail.com>
2024-08-07 18:03:37 +0200

    unbind s

--- init.lua
+++ init.lua
@@ -110,6 +110,7 @@ vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+vim.keymap.set('n', 's', '')
 
 -- center search
 vim.keymap.set('n', 'n', 'nzzzv')
@@ -666,6 +667,7 @@ require('lazy').setup({
             },
           },
         },
+        experimental,
       }
 
       -- Ensure the servers and tools above are installed
@@ -908,6 +910,9 @@ require('lazy').setup({
             cmp.config.compare.length,
             cmp.config.compare.order,
           },
+        },
+        experimental = {
+          ghost_text = false,
         },
       }
     end,
