01479d4173ed26b0e73013b25ac8b5a8c5ab3b03
chuchelov <chucheloff@gmail.com>
2024-12-09 21:33:11 +0500

    dot

--- init.lua
+++ init.lua
@@ -641,7 +641,7 @@ require('lazy').setup({
       local capabilities = require('blink.cmp').get_lsp_capabilities()
 
       -- Enable the following language servers
-      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+      --  Feel free to add/remove any LSPs that you want here. They will automatically be installed
       --
       --  Add any additional override configuration in the following tables. Available keys are:
       --  - cmd (table): Override the default command used to start the server
